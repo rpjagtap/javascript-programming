509ac9ac862151f5769bdc203ec2f816fc871014
ratnadeepjagtap <188825352+rpjagtap@users.noreply.github.com>
2025-05-06 09:25:45 +0530

    creating variable and datatype files
    creating variable and datatype files

--- 01-variable.js
+++ 01-variable.js
@@ -1,3 +1,43 @@
 
 
 console.log("================ Variable ====================");
+var myFullName;
+console.log(" Before initialization My Full Name is:",myFullName);
+myFullName="Ratnadeep Jagtap"
+console.log("After initialization My Full Name is:",myFullName);
+console.log("========Update variable Value=======");
+myFullName="Ratnadeep Prakashrao Jagtap";
+console.log("My Full Name is:",myFullName);
+console.log("=======Declare and initialize  variable in one line======= ");
+var myLove=" I Love Only JS"
+console.log(myLove);
+console.log("====Declare and initializing variables========");
+var firstName="Ratnadeep";
+console.log("First Name:",firstName);
+var lastName="Jagtap";
+console.log("Last Name:",lastName);
+var age=30;
+console.log("Age:",age);
+var address="Pune";
+console.log("Address:",address);
+var pinCode=411018;
+console.log("PinCode:",pinCode);
+var gender="Male";
+console.log("Gender",gender);
+var isMarried=false;
+console.log("IsMarried:",false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
